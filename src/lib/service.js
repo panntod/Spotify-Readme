943c50c1efc4752a95d398a092911128b597520d
panntod <munjalindra.pandhu@gmail.com>
2025-02-19 08:46:58 +0700

    refactor: Enhance SVG generation for Spotify card with improved styling and animation

--- src/lib/service.js
+++ src/lib/service.js
@@ -8,35 +8,124 @@ function generateSpotifyCard({ externalLink, cardImg, cardTitle, cardSubtitle, p
   const assetsPath = path.resolve('assets')
   const logoSvg = readFileSync(path.join(assetsPath, 'spotify-logo.svg'), { encoding: 'base64' })
 
-  return `
-  <svg fill="none" viewBox="0 0 300 150" width="300" height="150" xmlns="http://www.w3.org/2000/svg">
-    <foreignObject width="100%" height="100%">
-      <div xmlns="http://www.w3.org/1999/xhtml">
-        <style>
-          * { box-sizing: border-box; }
-          .external-link { text-decoration: none; display: flex; width: 300px; height: 150px; border-radius: 8px; overflow: hidden; background-color: #222; transition: transform 0.3s; }
-          .card { display: flex; width: 100%; height: 100%; }
-          .card__img { flex: 0 0 50%; background-image: ${cardImg}; background-repeat: no-repeat; background-size: cover; border-top-left-radius: 8px; border-bottom-left-radius: 8px; }
-          .card__body { flex: 0 0 50%; display: flex; position: relative; flex-direction: column; justify-content: center; padding: 12px; }
-          .card__logo { width: 80px; height: 40px; object-fit: contain; position: absolute; top: 0; left: 50%; transform: translateX(-50%); }
-          .card__title { font: 600 15px 'Segoe UI', Ubuntu, Sans-Serif; color: #fff; margin-bottom: 4px; }
-          .card__subtitle { font: 600 10px 'Segoe UI', Ubuntu, Sans-Serif; color: #fff; }
-          .overlay { position: absolute; right: 0; top: 0; left: 0; bottom: 0; background-image: linear-gradient(transparent 0%, rgba(30, 215, 96, 0.1) 50%); background-size: 150px 2px; pointer-events: none; }
-        </style>
-        <a class="external-link" href="${externalLink}" target="_blank">
-          <div class="card">
-            <div class="card__img"></div>
-            <div class="card__body">
-              <img class="card__logo" src="data:image/svg+xml;base64,${logoSvg}" />
-              <div class="card__title">${cardTitle}</div>
-              <div class="card__subtitle">${cardSubtitle}</div>
-            </div>
-            <div class="${playing ? '' : 'overlay'}"></div>
+  return `<svg fill="none" viewBox="0 0 300 150" width="300" height="150" xmlns="http://www.w3.org/2000/svg">
+  <foreignObject width="100%" height="100%">
+    <div xmlns="http://www.w3.org/1999/xhtml">
+      <style>
+        * {
+          box-sizing: border-box;
+        }
+
+        .external-link {
+          text-decoration: none;
+          display: flex;
+          width: 300px;
+          height: 150px;
+          border-radius: 8px;
+          overflow: hidden;
+          background-color: #222;
+          transition: transform 0.3s;
+        }
+
+        .card {
+          display: flex;
+          width: 100%;
+          height: 100%;
+        }
+
+        .card__img {
+          flex: 0 0 50%;
+          background-image: ${cardImg};
+          background-repeat: no-repeat;
+          background-size: cover;
+          border-top-left-radius: 8px;
+          border-bottom-left-radius: 8px;
+        }
+
+        .card__body {
+          flex: 0 0 50%;
+          display: flex;
+          position: relative;
+          flex-direction: column;
+          justify-content: center;
+          padding: 12px;
+        }
+
+        .card__logo {
+          width: 80px;
+          height: 40px;
+          object-fit: contain;
+          position: absolute;
+          top:0;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+
+        .card__title {
+          font: 600 15px 'Segoe UI', Ubuntu, Sans-Serif;
+          color: #fff;
+          overflow: hidden;
+          white-space: wrap;
+          animation: ${cardSubtitle ? 'none' : 'noise 2s infinite linear'};
+          margin-bottom: 4px;
+        }
+        
+        .card__subtitle {
+          font: 600 10px 'Segoe UI', Ubuntu, Sans-Serif;
+          color: #fff;
+          overflow: hidden;
+          white-space: wrap;
+        }
+
+        .overlay {
+          position: absolute;
+          right: 0;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          background-image: linear-gradient(transparent 0%, rgba(30, 215, 96, 0.1) 50%);
+          background-size: 150px 2px;
+          content: '';
+          pointer-events: none;
+        }
+
+        @keyframes noise {
+          0%, 3%, 5%, 42%, 44%, 63%, 65%, 92%, 94%, 100% {
+            opacity: 1;
+            transform: scaleY(1);
+          }
+          4.3% {
+            opacity: 1;
+            transform: scaleY(4);
+          }
+          43% {
+            opacity: 1;
+            transform: scaleX(10) rotate(60deg);
+          }
+          64.3% {
+            opacity: 1;
+            transform: scaleY(4);
+          }
+          93% {
+            opacity: 1;
+            transform: scaleX(20) rotate(-60deg);
+          }
+        }
+      </style>
+      <a class="external-link" href="${externalLink}" target="_blank">
+        <div class="card">
+          <div class="card__img"></div>
+          <div class="card__body">
+            <img class="card__logo" src="data:image/svg+xml;base64,${logoSvg}" />
+            <div class="card__title"><![CDATA[${cardTitle}]]></div>
+            <div class="card__subtitle"><![CDATA[${cardSubtitle}]]></div>
           </div>
-        </a>
-      </div>
-    </foreignObject>
-  </svg>`
+          <div class="${playing ? '' : 'overlay'}"></div>
+        </div>
+      </a>
+    </div>
+  </foreignObject>
+</svg>`
 }
 
 async function generateSpotifyCardData() {
